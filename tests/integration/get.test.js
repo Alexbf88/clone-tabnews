ce2d72d5dc22d94983a9d630a700a787f08dc1f2
Alexbf88 <109435161+Alexbf88@users.noreply.github.com>
2025-10-25 17:09:19 +0000

    adicionado databsae.js

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,6 +1,6 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch(
-    "https://crispy-garbanzo-qjpvqp5vx462xpv6-3000.app.github.dev/api/v1/status",
+    "https://shocking-goblin-wpgv7gqv5x7fgx46-3000.app.github.dev/",
   );
   expect(response.status).toBe(200);
 });
