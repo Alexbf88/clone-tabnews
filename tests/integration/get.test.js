a31bce7d4e2072074149ac990b5bef2bf1a55baa
Alexbf88 <109435161+Alexbf88@users.noreply.github.com>
2025-10-25 18:13:51 +0000

    adiciona os arquivos .env, arruma o link do test

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,6 +1,6 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch(
-    "https://shocking-goblin-wpgv7gqv5x7fgx46-3000.app.github.dev/",
+    "https://shocking-goblin-wpgv7gqv5x7fgx46-3000.app.github.dev/api/v1/status",
   );
   expect(response.status).toBe(200);
 });
